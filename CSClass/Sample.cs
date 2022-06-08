9ed2aadcedfb404ab96daa319c0afb7505edbc28
soooomm <soooomm00@gmail.com>
2022-06-08 09:36:36 +0900

    #12-22 생성자 : 정적 생성자

---
+++ CSClass/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSClass
+{
+    internal class Sample
+    {
+        internal static int value=10;
+
+        static Sample()
+        {
+            value = 20;
+            Console.WriteLine("정적 생성자 호출");
+        }
+    }
+}
